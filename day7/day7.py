256cf757a66e799c52e3906ac1ca808cf2bf35f9
hatch <hatch@quakebag.com>
2021-12-07 06:15:50 +0100

    day7

---
+++ day7/day7.py
@@ -0,0 +1,24 @@
+def get_crabs(lines: []):
+    return list(map(int, lines.split(',')))
+
+
+def part2(c: int):
+    return sum(range(1, c + 1))
+
+
+def day7():
+    with open('day7.txt') as data:
+        crabs = get_crabs(data.read())
+
+        least_fuel: int = False
+        for i in range(min(crabs), max(crabs) + 1):
+            fuel = 0
+            for c in crabs:
+                fuel += part2(abs(c - i))
+            if not least_fuel or fuel < least_fuel:
+                least_fuel = fuel
+        print(least_fuel)
+
+
+if __name__ == '__main__':
+    day7()
