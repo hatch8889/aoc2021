1ff6663c4223e7d21d7971e1d843dcc56f65ca1e
hatch <hatch@quakebag.com>
2021-12-11 14:01:29 +0100

    day11

--- day11/day11.py
+++ day11/day11.py
@@ -1,14 +1,49 @@
 import numpy as np
+import pygame
+
+zoom = 50
+
+
+def get_color(i):
+    return i*20, i*20, i*20
+
+
+def paint(screen, data, i):
+    screen.fill((0, 0, 0))
+
+    if np.count_nonzero(data > 0) == 0:
+        return
+
+    for x, y in np.nditer(np.where(data > 0)):
+        pygame.draw.rect(screen, get_color(data[x, y]), pygame.Rect(y * zoom, x * zoom, zoom, zoom))
+
+    font = pygame.font.Font(None, 80)
+    text = font.render(f"{i}", True, (200, 55, 20))
+    tt = zoom * 10 - text.get_height() - 10
+    screen.blit(text, (10, tt))
+
+    pygame.display.flip()
+    pygame.time.delay(250)
 
 
 def day10():
-    data = np.loadtxt('day11.txt', dtype=(int, int), converters={0: list}, encoding='latin1')
+    pygame.init()
+    screen = pygame.display.set_mode((zoom*10, zoom*10))
+    done = False
+    pygame.display.flip()
+
+    def get_data():
+        return np.loadtxt('day11.txt', dtype=(int, int), converters={0: list}, encoding='latin1')
+
+    data = get_data()
     all_flashes = 0
 
     def pos(n):
         return n if n >= 0 else 0
 
-    for i in range(1, 1000):
+    i = 0
+    while not done:
+        i = i + 1
         data += 1
         while True:
             if np.count_nonzero(data >= 10):
@@ -21,14 +56,21 @@ def day10():
             else:
                 break
 
+        paint(screen, data, i)
+
         f = np.count_nonzero(data == 0)
         all_flashes += f
         if i == 100:
             print(f"part1: {all_flashes}")
 
         if f == 100:
+            i = 0
             print(f"part2: {i}")
-            break
+            data = get_data()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
 
 if __name__ == '__main__':
