5366a1afc76def8abd77d33003cb2bf52003b067
hatch <hatch@quakebag.com>
2021-12-17 16:35:39 +0100

    multithread

--- day17/day17.py
+++ day17/day17.py
@@ -1,3 +1,6 @@
+import asyncio
+import concurrent.futures
+
 def step(position, velocity):
     x, y = position
     vx, vy = velocity
@@ -38,24 +41,47 @@ def try_hit(velocity, target_area):
 
         pos, vel = step(pos, vel)
 
-    return hit, max_y
+    return hit, max_y, velocity
+
+
+def inter(vx, target_area):
+    ret = []
+    for vy in range(-400, 400):
+        hit, max_y, velocity = try_hit((vx, vy), target_area)
+        if hit == 1:
+            ret.append((hit, max_y, velocity))
+    return ret
 
 
 def day17():
+    th_executor = concurrent.futures.ThreadPoolExecutor(max_workers=24)
+    event_loop = asyncio.get_event_loop()
+
     my_target_area = (155, 215, -132, -72)
     target_area = (20, 30, -10, -5)
     max_y = -500
     best_v = (0, 0)
     count = 0
+    tasks = []
+
     for vx in range(0, 400):
-        for vy in range(-400, 400):
-            h, my = try_hit((vx, vy), my_target_area)
+        task = event_loop.run_in_executor(th_executor, inter, vx, my_target_area)
+        tasks.append(task)
+
+    completed = asyncio.wait(tasks)
+    event_loop.run_until_complete(completed)
+
+    for t in tasks:
+        for result in t.result():
+            h, my, velocity = result
             if h == 1:
                 count += 1
                 if my > max_y:
                     max_y = my
-                    best_v = (vx, vy)
+                    best_v = velocity
+
     print(f" max-y: {max_y} ---- best velocity: {best_v} --- count {count}")
+
     # target area: x=155..215, y=-132..-72
     # target area: x=20..30, y=-10..-5
 
